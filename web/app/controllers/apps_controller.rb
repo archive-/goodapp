b6c589b89d04535fc408b503d77bfe707cea1bb1
TJ Koblentz <tj.koblentz@gmail.com>
2012-03-08 11:03:03 -0800

    small changes to main page

--- web/app/controllers/apps_controller.rb
+++ web/app/controllers/apps_controller.rb
@@ -7,6 +7,19 @@ class AppsController < ApplicationController
     @app = App.find(params[:id])
   end
 
+  def new
+    @app = App.new
+  end
+
+  def create
+    @app = App.new(params[:app])
+    if @app.save
+
+    else
+
+    end
+  end
+
   def flag
   end
 end
