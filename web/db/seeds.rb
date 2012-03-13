46edd41233a608b97327097423ace7fc0dd71ad2
Yuliaaa <skiswithtwotips@gmail.com>
2012-03-12 20:53:07 -0700

    seed tests updated and working now, create random devs. next step is apps

--- web/db/seeds.rb
+++ web/db/seeds.rb
@@ -5,9 +5,8 @@ User.populate(5) do |user|
   user.name = Faker::Name.name
   user.email = Faker::Internet.email
   user.password_salt = BCrypt::Engine.generate_salt
-  user.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
-  rand = 0..10
-  user.is_dev = rand < 7 
+  user.password_hash = BCrypt::Engine.hash_secret(password, user.password_salt)
+  user.is_dev = Random.rand(10) < 4 
   user.about = Populator.sentences(2..4)
 end
 
