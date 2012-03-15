7cc53e1ffefa88a7f270f1370d6f7c0c2ad31a58
TJ Koblentz <tj.koblentz@gmail.com>
2012-03-15 01:29:31 -0700

    horrible work... just trying to get stuff a bit better for the demo but im tired and sick. gg. good night

--- web/app/helpers/users_helper.rb
+++ web/app/helpers/users_helper.rb
@@ -2,4 +2,8 @@ module UsersHelper
   def its_you
     @user == current_user
   end
+
+  def user_show(user)
+    raw("<a href=\"#{user_url(user)}\" rel=\"tooltip\" class=\"tooltipped\" title=\"#{user.name}\">#{image_tag user.avatar.url(:thumb)}</a>")
+  end
 end
