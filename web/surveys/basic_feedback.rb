b8af18c767db93ccbad5ad58ad3a00c5fa645b9a
Yuliaaa <skiswithtwotips@gmail.com>
2012-04-10 22:56:47 -0700

    working on survey [basic/technical feedback] content stuff

--- web/surveys/basic_feedback.rb
+++ web/surveys/basic_feedback.rb
@@ -1,13 +1,26 @@
 survey "Feedback" do
   section 'Basic' do
     # A label is a question that accepts no answers
-    label "Please answer the following question"
+    label "Please answer the following questions."
 
-    # A basic question with radio buttons
-    q_1 "Did you feel tricked by application?", :pick => :one
-    answer "Very much"
-    answer "Sort of"
-    answer "Not really"
-    answer "No"
+    # "question" and "answer" may be abbreviated as "q" and "a"
+    q_1 "Select ALL Good Characterictics:", :pick => :any
+    a_1 "Good API"
+    a_2 "Fast and Reliable"
+    a_3 "Easy to use"
+    a_4 "Updates are consistent and improve performence"
+    a_5 "Works online and offline"
+
+    q_2 "Select ALL Bad Characterictics:", :pick => :any
+    a_1 "Drains battery"
+    a_2 "Contains a lot of advertisements"
+    a_3 "Uses services of phone without permission"
+    a_4 "Crashes often"
+    a_5 "Steals private info"
+    a_6 "Triggers unexpected sound/vibrate"
+    a_7 "Updates often cause problems"
+    a_8 "Uses too much internet"
+    a_9 "Charges account without concent"
+
   end
 end
---
+++ web/surveys/technical_feedback.rb
@@ -0,0 +1,87 @@
+survey "Techincal feedback" do
+  section "Basic questions" do
+
+    q_1 "How often do you use this application?", :pick => :one
+    a_1 "Multiple times per day"
+    a_2 "Couple of times a week"
+    a_3 "Couple of times a month"
+    a_4 "Rarely"
+    a_6 :other
+
+    q_2 "Was the App downloaded and run successfully? ", :pick => :one, :display_type => :inline
+    a_1 "yes"
+    a_2 "no"
+    a_3 "I don't know"
+
+    q_2a "select at least one of the following:", :pick => :any
+    a_1 "crashed at download"
+    a_2 "not able to use it at all after download"
+    a_3 "crashed at first run"
+    dependency :rule => "A"
+    condition_A :q_2, "==", :a_2
+
+    q_3 "Did the App behave as you expected it to? ", :pick => :one, :display_type => :inline
+    a_1 "yes"
+    a_2 "no"
+    a_3 "I don't know"
+
+    q_3a "select at least one of the following:", :pick => :any
+    a_1 "didn’t run at all"
+    a_2 "crashes right when clicked on"
+    a_3 "crashes a lot and makes it really hard to use"
+    a_4 "opened a completely different App"
+    a_5 "caused phone to freeze"
+    a_6 "cause phone to install an App without consent"
+    dependency :rule => "A"
+    condition_A :q_3, "==", :a_2    
+
+    q_4 "Did the App behave according to it’s description?", :pick => :one, :display_type => :inline
+    a_1 "yes"
+    a_2 "no"
+    a_3 "I don't know"  
+    
+    q_4a "select at least one of the following:", :pick => :any
+    a_1 "connected to the Internet"
+    a_2 "got a hold of your personal data with no permission"
+    a_3 "description doesn’t match to actual App behavior"
+    a_4 "the App charged you money without you consent"
+    a_5 "App uses a lot of battery power"
+    dependency :rule => "A"
+    condition_A :q_4, "==", :a_2          
+
+    # When an is_exclusive answer is checked, it unchecks all other options and disables them (using Javascript)
+    q_4 "Choose your favorite meats", :display_type => :inline, :pick => :any
+    a "Chicken"
+    a "Pork"
+    a "Beef"
+    a "Shellfish"
+    a "Fish"
+    a "I don't eat meats!!!", :is_exclusive => true
+    
+    grid "Tell us how often do you cover these each day" do
+      a "1"
+      a "2"
+      a "3"
+      a "4"
+      a "5"
+      a "6"
+      a "7"
+      a "8"
+      a "9"
+      a "10"
+      q "Security", :pick => :one
+      q "Connectivity", :pick => :one
+      q "Responsivness", :pick => :one
+    end
+       
+    # A basic question with checkboxes
+    # "question" and "answer" may be abbreviated as "q" and "a"
+    q "Overall score for the app", :pick => :any
+    a_1 "1 star"
+    a_2 "2 stars"
+    a_3 "3 starts"
+    a_4 "4 stars"
+    a_5 "5 stars"
+    a :omit  
+  end            
+end
