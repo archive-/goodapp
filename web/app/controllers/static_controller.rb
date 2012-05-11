984153d384d02512e6db62b08262d8676f74290d
Yuliaaa <skiswithtwotips@gmail.com>
2012-05-10 23:03:47 -0700

    added contact form validation/and fixed it so it sends the email now. should work now if you fill out the contact form

--- web/app/controllers/static_controller.rb
+++ web/app/controllers/static_controller.rb
@@ -34,8 +34,32 @@ class StaticController < ApplicationController
 
   def send_contact_mail
     # FIX THIS -- need validations -- make form_for with mail object?
-    UserMailer.send_contact_mail(params[:email], params[:name], params[:message])
-    redirect_to root_url, :notice => 'Message successfully sent.'
+    if params[:email] && !params[:email].blank?
+      @email = params[:email]
+    else
+      flash[:error] = 'You must enter an email address!'
+      redirect_to :action => 'contact'
+      return
+    end    
+    if params[:name] && !params[:name].blank?
+      @name = params[:name]
+    else
+      flash[:error] = 'You must give a name!'
+      redirect_to :action => 'contact'
+      return
+    end
+    if params[:message] && !params[:message].blank?
+      @message = params[:name]
+    else
+      flash[:error] = 'You must enter a message!'
+      redirect_to :action => 'contact'
+      return
+    end
+    if UserMailer.send_contact_mail(@email, @name, @message)
+      redirect_to root_url, :notice => 'Message successfully sent.'
+    else 
+      redirect_to root_url, :notice => 'Message was not sent.'
+    end      
   end
 
   def faq
--- web/app/mailers/user_mailer.rb
+++ web/app/mailers/user_mailer.rb
@@ -1,12 +1,11 @@
 class UserMailer < ActionMailer::Base
   # default :to => "example@goodapp.com"
-  default :to => "tj.koblentz@gmail.com"
+  default :to => "yulia.dubinina@gmail.com"
 
   def send_contact_mail(from_email, from_name, message)
     body = message
     mail(:subject => "Contact Form filled out by #{from_name}",
-         :reply_to => from_email,
-         :date => Time.now).deliver
+         :from => from_email, :body => message).deliver
   end
 
 end
