78dfb031ffea17156289da1e5d7aa6ff554cdc58
TJ Koblentz <tj.koblentz@gmail.com>
2012-05-19 08:19:02 -0700

    added a the static api page. going on a silly fieldtrip for earth class today -- will continue when i get back.

--- web2/app/controllers/static_controller.rb
+++ web2/app/controllers/static_controller.rb
@@ -1,4 +1,4 @@
 class StaticController < ApplicationController
-  def index
-  end
+  def index ; end
+  def api ; end
 end
--- web2/config/routes.rb
+++ web2/config/routes.rb
@@ -1,5 +1,6 @@
 Web2::Application.routes.draw do
   root to: "static#index"
+  get "/api" => "static#api"
   devise_for :users
   resources :users do
     resources :keys
