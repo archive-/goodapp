1604de47abb77dc3798ec03f715b1a0bc5a7a564
TJ Koblentz <tj.koblentz@gmail.com>
2012-05-20 12:24:33 -0700

    updated routes to make apps#new /upload and users#edit /settings to remove the illusions of changing /user/:id/edit to some other id.

--- web2/app/controllers/users_controller.rb
+++ web2/app/controllers/users_controller.rb
@@ -6,4 +6,15 @@ class UsersController < ApplicationController
   def edit
     @user = current_user
   end
+
+  def update
+    @user = current_user
+    if current_user.update_attributes(params[:user])
+      flash[:notice] = "Successfully updated your settings."
+      redirect_to referer
+    else
+      flash[:alert] = "There was an issue in updating your settings."
+      redirect_to referer
+    end
+  end
 end
--- web2/config/routes.rb
+++ web2/config/routes.rb
@@ -2,8 +2,12 @@ Web2::Application.routes.draw do
   root to: "static#index"
   get "/api" => "static#api"
   devise_for :users
-  resources :users do
+  resources :users, except: [:edit, :update] do
     resources :keys
-    resources :apps
+    resources :apps, except: [:new, :create]
   end
+  get "/settings" => "users#edit", as: :edit_user
+  put "/settings" => "users#update", as: :users
+  get "/upload" => "apps#new", as: :new_app
+  post "/upload" => "apps#create", as: :apps
 end
