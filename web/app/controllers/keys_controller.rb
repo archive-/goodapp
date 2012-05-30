2930f2fbc0daa241520ee810aae43a669f99b811
TJ Koblentz <tj.koblentz@gmail.com>
2012-05-30 13:07:30 -0700

    added automatic key creationg when verifying non-email key. also reformatted all redirect_to and render to return + inline alert/notice flash message setting

--- web/app/controllers/keys_controller.rb
+++ web/app/controllers/keys_controller.rb
@@ -8,27 +8,25 @@ class KeysController < ApplicationController
       @key.progress(60, "Email: Sent confirmation email to <#{@key.kee}>.")
       # TODO validate email?
       KeyMailer.email(@key.kee, @key.id).deliver
-      flash[:notice] = "Successfully upload your key to GoodApp. Processing now (see status below)."
-      redirect_to settings_path(anchor: "keys-pane")
+      return redirect_to settings_path(anchor: "keys-pane"),
+        notice: "Successfully upload your key to GoodApp. Processing now (see status below)."
     else
       if @key.title.blank?
         # TODO TODO [in lots of places] needs to be render!!
-        flash[:alert] = "You must give your key a title."
-        redirect_to settings_path(anchor: "keys-pane")
-        return
+        return redirect_to settings_path(anchor: "keys-pane"),
+          alert: "You must give your key a title."
       end
       if @key.kee.blank?
         # TODO TODO [in lots of places] needs to be render!!
-        flash[:alert] = "The key cannot be blank!"
-        redirect_to settings_path(anchor: "keys-pane")
-        return
+        return redirect_to settings_path(anchor: "keys-pane"),
+          alert: "The key cannot be blank!"
       end
       if @key.handle_upload
-        flash[:notice] = "Successfully upload your key to GoodApp. Processing now (see status below)."
-        redirect_to settings_path(anchor: "keys-pane")
+        return redirect_to settings_path(anchor: "keys-pane"),
+          notice: "Successfully upload your key to GoodApp. Processing now (see status below)."
       else
-        flash[:alert] = "There was an error in creating the key."
-        redirect_to settings_path(anchor: "keys-pane")
+        return redirect_to settings_path(anchor: "keys-pane"),
+          alert: "There was an error in creating the key."
       end
     end
   end
@@ -43,21 +41,25 @@ class KeysController < ApplicationController
   def confirm
     # TODO make sure they are logged in for this action
     @key = Key.find(params[:id])
+    # TODO check @key (is owned by current_user, etc.)
     if @key.confirmed_at
-      flash[:notice] = "Key already confirmed!"
-      redirect_to settings_path(anchor: "keys-pane")
-      return
+      return redirect_to settings_path(anchor: "keys-pane"),
+        alert: "Key already confirmed!"
     end
-    # TODO check key
     if @key.confirmation_token == params[:confirmation_token]
-      # TODO if key.style != :email, Key.create email key
+      if !@key.email_key?
+        # create email key for email used as confirmation
+        Key.create({user_id: @key.user_id, title: @key.confirmation_email,
+                   kee: @key.confirmation_email, status: 100, proper: true,
+                   style: :email, confirmed_at: Time.now}, without_protection: true)
+      end
       @key.confirmed_at = Time.now ; @key.save
       @key.progress(100)
-      flash[:notice] = "Key successfully confirmed."
-      redirect_to settings_path(anchor: "keys-pane")
+      return redirect_to settings_path(anchor: "keys-pane"),
+        notice: "Key successfully confirmed."
     else
-      flash[:alert] = "Key was not confirmed properly (improper confirmation token)."
-      redirect_to settings_path(anchor: "keys-pane")
+      return redirect_to settings_path(anchor: "keys-pane"),
+        alert: "Key was not confirmed properly (improper confirmation token)."
     end
   end
 
