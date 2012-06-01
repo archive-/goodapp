b90e06fea5786aec89080c76ede277e98075b9d9
TJ Koblentz <tj.koblentz@gmail.com>
2012-06-01 13:56:52 -0700

    added validations and a little security to endorsement model

--- web/app/models/endorsement.rb
+++ web/app/models/endorsement.rb
@@ -1,4 +1,15 @@
 class Endorsement < ActiveRecord::Base
   belongs_to :endorser, class_name: "User"
   belongs_to :endorsee, class_name: "User"
+
+  attr_accessible :endorsee_id, :comment
+
+  validates :endorsee_id,
+    presence: true,
+    uniqueness: {scope: :endorser_id}
+  validate :not_yourself
+
+  def not_yourself
+    errors.add(:endorsee_id, "can't be yourself") if endorser_id == endorsee_id
+  end
 end
