07e3ac28ad68384cb97dbba103409c7e1b9feab1
TJ Koblentz <tj.koblentz@gmail.com>
2012-06-01 13:56:23 -0700

    reformatted redirect_to to return and have notice/alert on next line. also added info to failed endorsements

--- web/app/controllers/endorsements_controller.rb
+++ web/app/controllers/endorsements_controller.rb
@@ -3,10 +3,11 @@ class EndorsementsController < ApplicationController
     @endorsement = Endorsement.new(params[:endorsement])
     @endorsement.endorser_id = current_user.id
     if @endorsement.save
-      redirect_to referer, notice: "Successfully endorsed #{@endorsement.endorsee.name}."
+      return redirect_to referer,
+        notice: "Successfully endorsed #{@endorsement.endorsee.name}."
     else
-      # TODO render..
-      redirect_to referer, alert: "Could not endorse #{@endorsement.endorsee.name}."
+      return redirect_to referer,
+        alert: "You cannot endorse #{@endorsement.endorsee.name}. You can neither endorse yourself nor endorse others multiple times."
     end
   end
 end
