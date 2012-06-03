9c092a6f4fea5e650a6f9730da90baac562bdbb9
Jasper Fredrickson <jasper@Jaspers-MacBook-Pro.local>
2012-06-03 02:43:33 -0700

    logic was incorrect, my bad. way to find the bug tj.

--- scripts/trust.cpp
+++ scripts/trust.cpp
@@ -172,7 +172,7 @@ int main()
     if(member_id == -1 || trusted_id == -1)
       break;
 
-    if(old_id != member_id && old_trusted != trusted_id)
+    if(old_id != member_id || old_trusted != trusted_id)
     {
       old_id = member_id;
       old_trusted = trusted_id;
@@ -189,7 +189,7 @@ int main()
 
   }
   create_network((members), num_members);
-  //test_structure((members), num_members);
+  test_structure((members), num_members);
   print_structure((members), num_members);
   deallocate(members, num_members);
   return 0;
